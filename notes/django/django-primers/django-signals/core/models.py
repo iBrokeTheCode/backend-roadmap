30ba4dd8f462a928b43a4699a60107494cf9fcea
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-05-12 07:34:45 -0500

    chore: Add cv field to User model and register it

--- notes/django/django-primers/django-signals/core/models.py
+++ notes/django/django-primers/django-signals/core/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    cv = models.FileField(upload_to="cvs/", blank=True, null=True)
